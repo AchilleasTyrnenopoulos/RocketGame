5e584e2f3f8133b5f28d86884a3f7a494211c7c2
AchilleasTyrnenopoulos <68752518+AchilleasTyrnenopoulos@users.noreply.github.com>
2021-10-17 23:45:28 +0300

    Initial commit

---
+++ RocketGame/Assets/Scripts/RocketMovement.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class RocketMovement : MonoBehaviour
+{
+    public static RocketMovement instance;
+
+    //public event Action onThrustStart;
+
+    [SerializeField]
+    private Rigidbody rb;
+    [SerializeField]
+    private float thrustSpeed;
+
+    [SerializeField]
+    private float rotationSpeed;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        //add event listeners
+        //onThrustStart += Thrust;
+    }
+
+    private void OnDisable()
+    {
+        //onThrustStart -= Thrust;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //public void OnThrustStart()
+    //{
+    //    onThrustStart?.Invoke();
+    //}
+
+    public void Thrust()
+    {        
+        rb.AddRelativeForce(Vector3.up * thrustSpeed * Time.deltaTime);
+    }
+
+    public void Rotate(int left)
+    {
+        //freeze physics system rotation
+        rb.freezeRotation = true;
+        //apply 'our' rotation
+        this.transform.Rotate(Vector3.forward * left * rotationSpeed * Time.deltaTime);
+        //unfreeze physics system rotation
+        rb.freezeRotation = false;
+    }
+}
---
+++ RocketGame/Assets/Scripts/UserInputController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInputController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Input.GetKeyDown(KeyCode.Space))
+        //    OnThrustStart
+        //if (Input.GetKey(KeyCode.Space))
+        if(Input.GetMouseButton(0))
+            RocketMovement.instance.Thrust();
+
+        //rotation
+        if (Input.GetKey(KeyCode.A) )
+        {
+            RocketMovement.instance.Rotate(1);
+        }
+        else if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+        {
+            RocketMovement.instance.Rotate(-1);
+        }
+    }
+}
