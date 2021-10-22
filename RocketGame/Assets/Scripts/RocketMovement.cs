a39cdb871f4d867fc14fd051eea7cd12c9487291
AchilleasTyrnenopoulos <68752518+AchilleasTyrnenopoulos@users.noreply.github.com>
2021-10-22 23:10:29 +0300

    Rocket model
    - Add modular sci fi weapons package
    - Add rocket model
    - Change pivot point

--- RocketGame/Assets/Scripts/RocketMovement.cs
+++ RocketGame/Assets/Scripts/RocketMovement.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
 public class RocketMovement : MonoBehaviour
 {
     public static RocketMovement instance;
@@ -28,7 +27,7 @@ public class RocketMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponentInChildren<Rigidbody>();
         thrustAudioSrc = GetComponent<AudioSource>();
 
         //add event listeners
