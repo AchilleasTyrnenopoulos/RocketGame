1a4801ecba4bea35b0c2f85f6ea5d425e72c85bb
AchilleasTyrnenopoulos <68752518+AchilleasTyrnenopoulos@users.noreply.github.com>
2022-03-24 21:54:35 +0200

    Script execution order
    - Fix spawn rocket bug by changing the script execution order

--- RocketGame/Assets/Scripts/Spawners/RocketSpawner.cs
+++ RocketGame/Assets/Scripts/Spawners/RocketSpawner.cs
@@ -6,6 +6,8 @@ public class RocketSpawner : Spawner
 {
     private void Awake() 
     {
+        //if (GameData.instance == null)
+        //    print("this is the error");
         spawnPrefab = GameData.instance.rocketPrefab;
     }
 
